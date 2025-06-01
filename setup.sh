571a0855208ed54e186715a9e18273f57dc4aea8
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2025-06-02 01:05:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
 pip install -r requirements.txt
+pip install -U "huggingface_hub[cli]"
+huggingface-cli login --token $HUGGINGFACE_TOKEN
